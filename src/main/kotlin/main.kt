bd4bf94aef2a5be9bb2ee1c64bc0bc6dbe515d7b
Rawlin Crasto <rawlin@greedygame.com>
2021-03-12 13:45:26 +0530

    "`date`"

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,29 @@
+import java.util.*
+import kotlin.collections.HashSet
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val linkedList = LinkedList<Int>()
+    linkedList.addLast(1)
+    linkedList.addLast(2)
+    linkedList.addLast(2)
+    linkedList.addLast(3)
+    linkedList.addLast(4)
+    linkedList.addLast(5)
+    linkedList.addLast(6)
+    linkedList.addLast(5)
+    removeDuplicatesFromLinkedList(linkedList)
+    linkedList.forEach {
+        print("$it->")
+    }
+}
+
+private fun removeDuplicatesFromLinkedList(linkedList: LinkedList<Int>) {
+    val hashSet = HashSet<Int>()
+    linkedList.forEach { element ->
+        hashSet.add(element)
+    }
+    linkedList.clear()
+    hashSet.forEach { element ->
+        linkedList.add(element)
+    }
 }
