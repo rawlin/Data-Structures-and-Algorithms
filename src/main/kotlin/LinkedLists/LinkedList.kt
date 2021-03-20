9ffdd6dee4cbe9145bab44ae63390a2b29d85b1d
Rawlin <25471892+rawlin@users.noreply.github.com>
2021-03-20 22:12:28 +0530

    email change

---
+++ src/main/kotlin/LinkedLists/LinkedList.kt
@@ -0,0 +1,14 @@
+package LinkedLists
+
+class LinkedList<T> {
+
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+    private var size = 0
+
+    /**
+     * Add value to the front of the LinkedList
+     */
+
+}
+
